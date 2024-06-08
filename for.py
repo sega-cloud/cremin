8a8ef44ad0f89e636ac2af0405a31634783f9354
tester <test@test.ru>
2024-06-09 00:28:05 +0300

    test

--- for.py
+++ for.py
@@ -1,8 +1,10 @@
+import os
+import glob
 import json
 import tabula
 import requests
 
-# Функция для конвертации таблицы из PDF в JSON
+# Функция для конвертации таблицы из PDF 
 def convert_pdf_table_to_json(pdf_file_path, page_number):
     # Извлечение таблицы с указанной страницы
     df = tabula.read_pdf(pdf_file_path, pages=page_number, multiple_tables=True)[0]
@@ -17,19 +19,34 @@ def send_json_to_server(json_data, url):
     response = requests.post(url, data=json_data, headers=headers)
     return response
 
-# Пример использования функций
-pdf_path = '/Users/a1234/Downloads/pdf_by_any-pdf.com__637grseqa3x7c397gf633fcg_test.pdf'
-page_number = 1  # Номер страницы с таблицей
-json_result = convert_pdf_table_to_json(pdf_path, page_number)
+# Путь к папке, где ожидается наличие файла
+folder_path = '/Volumes/Update/ПДФ'
+# Шаблон поиска для файлов PDF
+pattern = '*.pdf'
 
-print(json_result)
+# Поиск всех PDF-файлов в папке
+pdf_files = glob.glob(os.path.join(folder_path, pattern))
 
-# URL сервера, на который вы хотите отправить JSON
-server_url = 'https://cremin.ru/cr-system/scenario/socet'
+# Проверка наличия PDF-файлов
+if pdf_files:
+    # Использование первого найденного PDF-файла
+    pdf_path = pdf_files[0]
+    page_number = 1  # Номер страницы с таблицей
+    json_result = convert_pdf_table_to_json(pdf_path, page_number)
+    print(json_result)
 
-# Отправка JSON на сервер
-response = send_json_to_server(json_result, server_url)
+    # URL сервера, на который вы хотите отправить JSON
+    server_url = 'https://cremin.ru/cr-system/scenario/socet'
+
+    # Отправка JSON на сервер
+    response = send_json_to_server(json_result, server_url)
 if response.status_code == 200:
     print('JSON успешно отправлен')
+    # Удаление файла после успешной отправки
+    try:
+        os.remove(pdf_path)
+        print(f'Файл {pdf_path} был успешно удален')
+    except OSError as e:
+        print(f'Ошибка при удалении файла {pdf_path}: {e}')
 else:
     print('Ошибка при отправке JSON:', response.status_code)
